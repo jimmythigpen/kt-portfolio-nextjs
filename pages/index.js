c005bfa8cf6dd24e1b9fd559372b81dca7ffe34e
Jimmy Thigpen <jmthigpen@me.com>
2021-10-10 21:04:44 -0600

    update config

--- pages/index.js
+++ pages/index.js
@@ -69,9 +69,9 @@ export default class Home extends Component {
           </div>
         </div>
         {
-          sectionData.sections.map((section, index) => {
-            console.log('section:', section);
-            return (
+          sectionData.sections.map((section, index) => (
+            <>
+              <a name={section.slug} />
               <div key={index} className={`content-section-${index % 2 ? 'a' : 'b'}`}>
                 <div className="container">
                   <div className="row">
@@ -87,93 +87,9 @@ export default class Home extends Component {
                   </div>
                 </div>
               </div>
-            )
-          })
+            </>
+          ))
         }
-
-        {/* <div className="col-lg-6 col-sm-push-6  col-sm-6"> */}
-
-        {/* <a name="about" />
-        <div className="content-section-b">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-sm-push-6  col-sm-6">
-                <hr className="section-heading-spacer" />
-                <div className="clearfix" />
-                <h2 className="section-heading">About</h2>
-                <p className="lead">Katie Thigpen, native of Charleston, SC, is a freelance trombonist and educator in the Washington, DC area. She received her Doctor of Musical Arts and Master of Music degrees from the University of Cincinnati, College-Conservatory of Music where she studied with Tim Anderson. She earned her Bachelor of Music degree in music education from the University of South Carolina where she studied with Brad Edwards and received the Robert Van Doren Award for Most Outstanding Senior in music education. Most recently she has studied with Per Brevig, faculty at The Juilliard School and retired principal trombonist of the Metropolitan Opera Orchestra, and Craig Mulcahy, principal trombonist of the National Symphony Orchestra and faculty at the University of Maryland.</p>
-              </div>
-              <div className="col-lg-6 col-sm-pull-6  col-sm-6">
-                <img className="img-responsive" src="bower_components/bootstrap-sass/assets/img/about.jpg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <a name="teaching" />
-        <div className="content-section-a">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-sm-6">
-                <hr className="section-heading-spacer" />
-                <div className="clearfix" />
-                <h2 className="section-heading">Teaching</h2>
-                <p className="lead">As an educator Dr. Thigpen maintains a studio of private students in the metropolitan DC area. She teaches low brass students at DeMatha Catholic High School, Holy Trinity Episcopal Day School, International School of Music, and with the DC Youth Orchestra Program. During the summer Dr. Thigpen is on faculty at Blue Lake Fine Arts Camp in Twin Lake, Michigan. Before joining the faculty at Blue Lake she was the director of their middle school camp, Camp Bernstein, and also worked as a camp counselor. Next summer will be her tenth at Blue Lake.</p>
-              </div>
-              <div className="col-lg-6 col-sm-6">
-                <img className="img-responsive" src="bower_components/bootstrap-sass/assets/img/teaching.jpg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div> */}
-
-        {/* <a name="about" />
-        <div className="content-section-b">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-sm-push-6  col-sm-6">
-                <hr className="section-heading-spacer" />
-                <div className="clearfix" />
-                <h2 className="section-heading">About</h2>
-                <p className="lead">Katie Thigpen, native of Charleston, SC, is a freelance trombonist and educator in the Washington, DC area. She received her Doctor of Musical Arts and Master of Music degrees from the University of Cincinnati, College-Conservatory of Music where she studied with Tim Anderson. She earned her Bachelor of Music degree in music education from the University of South Carolina where she studied with Brad Edwards and received the Robert Van Doren Award for Most Outstanding Senior in music education. Most recently she has studied with Per Brevig, faculty at The Juilliard School and retired principal trombonist of the Metropolitan Opera Orchestra, and Craig Mulcahy, principal trombonist of the National Symphony Orchestra and faculty at the University of Maryland.</p>
-              </div>
-              <div className="col-lg-6 col-sm-pull-6  col-sm-6">
-                <img className="img-responsive" src="bower_components/bootstrap-sass/assets/img/about.jpg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <a name="teaching" />
-        <div className="content-section-a">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-sm-6">
-                <hr className="section-heading-spacer" />
-                <div className="clearfix" />
-                <h2 className="section-heading">Teaching</h2>
-                <p className="lead">As an educator Dr. Thigpen maintains a studio of private students in the metropolitan DC area. She teaches low brass students at DeMatha Catholic High School, Holy Trinity Episcopal Day School, International School of Music, and with the DC Youth Orchestra Program. During the summer Dr. Thigpen is on faculty at Blue Lake Fine Arts Camp in Twin Lake, Michigan. Before joining the faculty at Blue Lake she was the director of their middle school camp, Camp Bernstein, and also worked as a camp counselor. Next summer will be her tenth at Blue Lake.</p>
-              </div>
-              <div className="col-lg-6 col-sm-6">
-                <img className="img-responsive" src="bower_components/bootstrap-sass/assets/img/teaching.jpg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <a name="performing" />
-        <div className="content-section-b">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-sm-push-6  col-sm-6">
-                <hr className="section-heading-spacer" />
-                <div className="clearfix" />
-                <h2 className="section-heading">Performing</h2>
-                <p className="lead">Dr. Thigpen performs regularly with various chamber groups and orchestras in the DC area. She has performed with the National Philharmonic, Roanoke Symphony Orchestra, Lancaster Symphony Orchestra, Cincinnati Symphony Orchestra, Cincinnati Ballet Orchestra, Dayton Philharmonic Orchestra, West Virginia Symphony, York Symphony Orchestra, Amadeus Orchestra, Great Noise Ensemble, Brass of the Potomac and the Ars Nova Chamber Orchestra. As a fellowship recipient she spent two summers at the Aspen Music Festival and School where she performed under the baton of several notable conductors such as David Robertson, Larry Rachleff, Leonard Slatkin, Nicholas McGegan, and Robert Spano. As a featured soloist with the Blue Lake Fine Arts Camp Staff Band in 2009 and 2010 Dr. Thigpen performed Paul Creston's Fantasy for Trombone and William Goldstein's Colloquy. In 2013 she won the Eastern Trombone Workshop National Solo Competition.</p>
-              </div>
-              <div className="col-lg-6 col-sm-pull-6  col-sm-6">
-                <img className="img-responsive" src="bower_components/bootstrap-sass/assets/img/performing.jpg" alt="" />
-              </div>
-            </div>
-          </div>
-        </div> */}
         <a name="links" />
         <div className="content-section-a">
           <div className="container">
